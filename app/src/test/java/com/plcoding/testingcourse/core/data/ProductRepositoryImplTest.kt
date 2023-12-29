66b54ef7c906ef37df43938abefa66122b15bc97
Cheng-Yu Chung <chengyu.chung@gosaico.com>
2023-12-30 03:08:52 +0800

    Mock Web Server & Retrofit

--- app/src/test/java/com/plcoding/testingcourse/core/data/ProductRepositoryImplTest.kt
+++ app/src/test/java/com/plcoding/testingcourse/core/data/ProductRepositoryImplTest.kt
@@ -1,6 +1,7 @@
 package com.plcoding.testingcourse.core.data
 
 import assertk.assertThat
+import assertk.assertions.isEqualTo
 import assertk.assertions.isTrue
 import com.plcoding.testingcourse.core.domain.LogParam
 import com.plcoding.testingcourse.core.domain.ProductRepository
@@ -9,23 +10,58 @@ import io.mockk.every
 import io.mockk.mockk
 import io.mockk.verify
 import kotlinx.coroutines.runBlocking
+import okhttp3.mockwebserver.MockResponse
+import okhttp3.mockwebserver.MockWebServer
 import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import retrofit2.HttpException
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.create
 
 internal class ProductRepositoryImplTest {
 
     private lateinit var repository: ProductRepositoryImpl
     private lateinit var productApi: ProductApi
     private lateinit var analyticsLogger: FirebaseAnalyticsLogger
+    private lateinit var mockWebServer: MockWebServer
 
     @BeforeEach
     fun setUp() {
-        productApi = mockk()
+        mockWebServer = MockWebServer()
+        // Create a mock Retrofit service
+        // but the base url actually does not connect to the internet, only in the local end
+        productApi = Retrofit.Builder()
+            .addConverterFactory(GsonConverterFactory.create())
+            .baseUrl(mockWebServer.url("/"))
+            .build()
+            .create()
+
         analyticsLogger = mockk(relaxed = true)
         repository = ProductRepositoryImpl(productApi, analyticsLogger)
     }
+
+    @Test
+    fun `Response error, exception logger - MockWebServer` () = runBlocking {
+        mockWebServer.enqueue(
+            MockResponse().setResponseCode(404)//Can mock more configurations in MockResponse() API if needed
+        )
+
+        val result = repository.purchaseProducts((listOf()))
+
+        assertThat(result.isFailure).isTrue()
+
+        verify {
+            analyticsLogger.logEvent(
+                "http_error",
+                LogParam("code",404),
+                LogParam("message", "Client Error")
+            )
+        }
+
+    }
+
 
     @Test
     fun `Response error, exception is logged`() = runBlocking {
